a1ae0b350581287c56b7ff965cc589ee2c421f44
3060s <78622374+3060s@users.noreply.github.com>
2023-12-29 13:11:35 +0100

    Dodaj pliki projektów.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Notepad__
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //textbox.IsReadOnly = true;
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Filter = "(*.txt, *.ini, *.cfg, *.xml, *.yml, *.json)|*.txt;*.ini;*.cfg;*.xml;*.yml;*.json";
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                textbox.Content = filename;
+            }
+        }
+
+        private void SaveFile(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveAsFile(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
